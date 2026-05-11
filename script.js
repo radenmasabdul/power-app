e505129455dabe8253572a3a5db2733a7f345135
radenmasabdul <abdul.tamsis@gmail.com>
2026-05-11 15:00:59 +0700

    feat: implement about section

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$(".menu-toggle").click(function () {
-  $(".nav-menu").toggleClass("active");
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((item) => item.classList.remove("active"));
+    link.classList.add("active");
+    navMenu.classList.remove("active");
+  });
 });
